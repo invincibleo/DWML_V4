90fb46e9560e15a7fc90c4ea0529139d9ef4f925
invincibleo <tangduowei@live.com>
2019-01-16 21:37:23 +0100

    data_provider done, checked the plotings, before shuffle and after, they seems right

--- data_generator.py
+++ data_generator.py
@@ -1,12 +1,118 @@
-# Created by invincibleo at 15/01/2019
-
-Feature:  #Enter feature name here
-# Enter feature description here
-
-Scenario:  # Enter scenario name here
-
 # @Time    : 15/01/2019
-# @Author  : invincibleo
+# @Author  : Duowei Tang
 # @Site    : https://iiw.kuleuven.be/onderzoek/emedia/people/phd-students/duoweitang
 # @File    : data_generator.py
 # @Software: PyCharm
+
+import tensorflow as tf
+import numpy as np
+import os
+import librosa
+import arff
+import matplotlib
+matplotlib.use("TkAgg")
+import matplotlib.pyplot as plt
+
+from tqdm import tqdm
+from pathlib import Path
+
+
+root_dir = Path("/Users/invincibleo/Box Sync/PhD/Experiment/Datasets/AVEC2016")
+
+portion_to_id = dict(
+    train=["train_1","train_2","train_3","train_4","train_5","train_6","train_7","train_8","train_9"],
+    valid=["dev_1","dev_2","dev_3","dev_4","dev_5","dev_6","dev_7","dev_8","dev_9"],
+    #test  = [54, 53, 13, 20, 22, 32, 38, 47, 48, 49, 57, 58, 59, 62, 63] # 54, 53
+)
+
+# The following functions can be used to convert a value to a type compatible
+# with tf.Example.
+def _str_feature(value):
+  """Returns a bytes_list from a string / byte."""
+  return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value.encode('utf-8')]))
+
+def _bytes_feature(value):
+  """Returns a bytes_list from a string / byte."""
+  return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+def _float_feature(value):
+  """Returns a float_list from a float / double."""
+  return tf.train.Feature(float_list=tf.train.FloatList(value=[value]))
+
+def _int64_feature(value):
+  """Returns an int64_list from a bool / enum / int / uint."""
+  return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+
+def _bytes_list_feature(value):
+  """Returns a bytes_list from a string / byte."""
+  return tf.train.Feature(bytes_list=tf.train.BytesList(value=value))
+
+def _float_list_feature(value):
+  """Returns a float_list from a float / double."""
+  return tf.train.Feature(float_list=tf.train.FloatList(value=value))
+
+def _int64_list_feature(value):
+  """Returns an int64_list from a bool / enum / int / uint."""
+  return tf.train.Feature(int64_list=tf.train.Int64List(value=value))
+
+# Modify features here
+def get_samples(subject_id):
+    arousal_label_path = root_dir / 'ratings_gold_standard/arousal/{}.arff'.format(subject_id)
+    valence_label_path = root_dir / 'ratings_gold_standard/valence/{}.arff'.format(subject_id)
+    try:
+        subsampled_audio, fs = librosa.load(str(root_dir / "recordings_audio/{}.wav".format(subject_id)), dtype='float32', sr=16000, mono=True)
+        audio_frames_org = np.reshape(subsampled_audio, (-1, int(0.04 * fs)))
+    except OSError as e:
+        print("When reading the audio, something wrong...")
+        print(e)
+        return []
+
+    audio_frames = []
+    for i in range(0, audio_frames_org.shape[0]):
+        audio = np.array(audio_frames_org[i, :])
+        audio = audio[:int(0.04*fs)]
+        audio_frames.append(audio.astype(np.float32))
+
+    arrf_content = arff.load(open(str(arousal_label_path), 'r'))
+    arousal = np.array(arrf_content['data'])
+    arousal = np.array(arousal[1:, 2:], dtype='float32')  # delete the first column which is the timesteps
+
+    arrf_content = arff.load(open(str(valence_label_path), 'r'))
+    valence = np.array(arrf_content['data'])
+    valence = np.array(valence[1:, 2:], dtype='float32')  # delete the first column which is the timesteps
+
+    return audio_frames, np.dstack([arousal.T, valence.T])[0].astype(np.float32)
+
+def serialize_sample(writer, subject_id):
+    subject_name = '{}'.format(subject_id)
+    for i, (audio, label) in enumerate(zip(*get_samples(subject_name))):
+        label_shape = label.shape
+        audio_shape = audio.shape
+        feature = {
+            'sample_id': _int64_feature(i),
+            'subject_id': _str_feature(subject_id),
+            'label': _float_list_feature(np.reshape(label, (-1,))),
+            'raw_audio': _float_list_feature(np.reshape(audio, (-1,))),
+            'label_shape': _int64_list_feature(label_shape),
+            'audio_shape': _int64_list_feature(audio_shape)
+        }
+
+        example = tf.train.Example(features=tf.train.Features(feature=feature))
+
+        writer.write(example.SerializeToString())
+        del audio, label
+
+def main(directory):
+    for portion in portion_to_id.keys():
+        for subj_id in tqdm(portion_to_id[portion], desc=portion):
+            if not os.path.exists(directory / 'tf_records' / portion):
+                os.makedirs(directory / 'tf_records' / portion)
+            writer = tf.python_io.TFRecordWriter(
+                (directory / 'tf_records' / portion / '{}.tfrecords'.format(subj_id)
+                ).as_posix())
+            serialize_sample(writer, subj_id)
+
+if __name__ == "__main__":
+  main(Path("./test_output_dir"))
+
+
--- data_provider.py
+++ data_provider.py
@@ -3,3 +3,78 @@
 # @Site    : https://iiw.kuleuven.be/onderzoek/emedia/people/phd-students/duoweitang
 # @File    : data_provider.py
 # @Software: PyCharm
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow as tf
+import numpy as np
+from pathlib import Path
+
+import matplotlib
+
+matplotlib.use("TkAgg")
+import matplotlib.pyplot as plt
+
+tf.enable_eager_execution()
+slim = tf.contrib.slim
+
+def parse_fn(example):
+  "Parse TFExample records and perform simple data augmentation."
+  example_fmt = {
+        'sample_id': tf.FixedLenFeature((), tf.int64),
+        'subject_id': tf.FixedLenFeature((), tf.string),
+        'label': tf.FixedLenFeature((2), tf.float32),
+        'raw_audio': tf.FixedLenFeature((640), tf.float32),
+        'label_shape': tf.FixedLenFeature((), tf.int64),
+        'audio_shape': tf.FixedLenFeature((), tf.int64),
+    }
+  features = tf.parse_single_example(example, example_fmt)
+  raw_audio = features['raw_audio']
+  label = features['label']
+
+  audio_shape = features['audio_shape']
+  label_shape = features['label_shape']
+
+  raw_audio = tf.reshape(raw_audio, (-1, audio_shape))
+  label = tf.reshape(label, (-1, label_shape))
+
+  return raw_audio, label
+
+def get_split(dataset_dir, is_training=True, split_name='train', batch_size=32,
+              seq_length=100, debugging=False):
+    """Returns a data split of the RECOLA dataset, which was saved in tfrecords format.
+
+    Args:
+        split_name: A train/test/valid split name.
+    Returns:
+        The raw audio examples and the corresponding arousal/valence
+        labels.
+    """
+
+    root_path = Path(dataset_dir) / split_name
+    paths = [str(x) for x in root_path.glob('*.tfrecords')]
+    paths.sort()
+
+    filename_queue = tf.data.Dataset.list_files(paths,
+                                                shuffle=is_training,
+                                                seed=None)
+
+    dataset = filename_queue.interleave(tf.data.TFRecordDataset, cycle_length=1)
+    dataset = dataset.map(map_func=parse_fn)
+    dataset = dataset.batch(batch_size=seq_length)
+    dataset = dataset.map(lambda x, y: (tf.transpose(x, [1, 0, 2]), y))
+    dataset = dataset.batch(batch_size=batch_size)
+    dataset = dataset.prefetch(buffer_size=20000/seq_length/batch_size)
+    if is_training:
+        dataset = dataset.shuffle(buffer_size=10000)
+
+    return dataset
+
+# def main(dataset_dir):
+#     get_split(dataset_dir, is_training=True, split_name='train',
+#               batch_size=32, seq_length=100, debugging=False)
+#
+# if __name__ == "__main__":
+#   main(Path("./test_output_dir/tf_records"))
