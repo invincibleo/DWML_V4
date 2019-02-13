49e90fdd140569b0007acbf27079ba6c46838a09
invincibleo <tangduowei@live.com>
2019-02-13 15:28:48 +0100

    try adding batchnormalization after dropouts

--- AE_train.py
+++ AE_train.py
@@ -49,13 +49,15 @@ def inference_net(audio_frames=None,
         net = tf.layers.Dense(4*num_features,
                               activation=tf.nn.relu)(audio_input)
         net = tf.layers.dropout(net, rate=0.5)
-        # net = tf.layers.batch_normalization(net)
+        net = tf.layers.batch_normalization(net)
         net = tf.layers.Dense(3*num_features,
                               activation=tf.nn.relu)(net)
         net = tf.layers.dropout(net, rate=0.5)
+        net = tf.layers.batch_normalization(net)
         net = tf.layers.Dense(2*num_features,
                               activation=tf.nn.relu)(net)
         net = tf.layers.dropout(net, rate=0.5)
+        net = tf.layers.batch_normalization(net)
         net = tf.layers.Dense(256,
                               activation=tf.nn.relu)(net)
 
@@ -72,12 +74,15 @@ def generative_net(audio_frames=None,
         net = tf.layers.Dense(2*num_features,
                               activation=tf.nn.relu)(net)
         net = tf.layers.dropout(net, rate=0.5)
+        net = tf.layers.batch_normalization(net)
         net = tf.layers.Dense(3*num_features,
                               activation=tf.nn.relu)(net)
         net = tf.layers.dropout(net, rate=0.5)
+        net = tf.layers.batch_normalization(net)
         net = tf.layers.Dense(4*num_features,
                               activation=tf.nn.relu)(net)
         net = tf.layers.dropout(net, rate=0.5)
+        net = tf.layers.batch_normalization(net)
         net = tf.layers.Dense(num_features)(net)
         net = tf.reshape(net, (-1, 1, seq_length, num_features))
         return net
---
+++ VAE_dense_train.py
@@ -0,0 +1,5 @@
+# @Time    : 13/02/2019
+# @Author  : invincibleo
+# @Site    : https://iiw.kuleuven.be/onderzoek/emedia/people/phd-students/duoweitang
+# @File    : VAE_dense_train.py
+# @Software: PyCharm
