fixed pca computation, and dropout layers the training parameter was not set, now is assigned with is_training

AE_train.py
@@ -47,15 +47,15 @@ def inference_net(audio_frames=None,
         audio_input = tf.reshape(audio_frames, [-1, 640])
         net = tf.layers.Dense(4*num_features,
                               activation=tf.nn.relu)(audio_input)
-        net = tf.layers.dropout(net, rate=0.5)
+        net = tf.layers.dropout(net, rate=0.5, training=is_training)
         # net = tf.layers.batch_normalization(net, training=is_training)
         net = tf.layers.Dense(3*num_features,
                               activation=tf.nn.relu)(net)
-        net = tf.layers.dropout(net, rate=0.5)
+        net = tf.layers.dropout(net, rate=0.5, training=is_training)
         # net = tf.layers.batch_normalization(net, training=is_training)
         net = tf.layers.Dense(2*num_features,
                               activation=tf.nn.relu)(net)
-        net = tf.layers.dropout(net, rate=0.5)
+        net = tf.layers.dropout(net, rate=0.5, training=is_training)
         # net = tf.layers.batch_normalization(net, training=is_training)
         net = tf.layers.Dense(latent_dim,
                               activation=tf.nn.relu)(net)
@@ -72,15 +72,15 @@ def generative_net(audio_frames=None,
         net = tf.reshape(audio_frames, (-1, latent_dim))
         net = tf.layers.Dense(2*num_features,
                               activation=tf.nn.relu)(net)
-        net = tf.layers.dropout(net, rate=0.5)
+        net = tf.layers.dropout(net, rate=0.5, training=is_training)
         # net = tf.layers.batch_normalization(net, training=is_training)
         net = tf.layers.Dense(3*num_features,
                               activation=tf.nn.relu)(net)
-        net = tf.layers.dropout(net, rate=0.5)
+        net = tf.layers.dropout(net, rate=0.5, training=is_training)
         # net = tf.layers.batch_normalization(net, training=is_training)
         net = tf.layers.Dense(4*num_features,
                               activation=tf.nn.relu)(net)
-        net = tf.layers.dropout(net, rate=0.5)
+        net = tf.layers.dropout(net, rate=0.5, training=is_training)
         # net = tf.layers.batch_normalization(net, training=is_training)
         net = tf.layers.Dense(num_features)(net)
         net = tf.reshape(net, (-1, 1, seq_length, num_features))
@@ -155,7 +155,7 @@ def train(dataset_dir=None,
             audio_input_reshaped = audio_input_reshaped - audio_input_mean
             audio_input_covariance = tf.matmul(audio_input_reshaped, audio_input_reshaped, transpose_a=True) / seq_length
             s, u, v = tf.svd(audio_input_covariance)
-            pca_low_dim = tf.matmul(tf.reshape(x_logit, (-1, seq_length, num_features)), u[:, :, :latent_dim])
+            pca_low_dim = tf.matmul(audio_input_reshaped, u[:, :, :latent_dim])
             reconstruction_pca = tf.matmul(pca_low_dim, u[:, :, :latent_dim], transpose_b=True) + audio_input_mean
 
         tf.summary.audio("reconstruction_audio",

GAN_train.py
@@ -41,13 +41,13 @@ def generator(audio_frames=None,
         audio_input = tf.reshape(audio_frames, [-1, 640])
         net = tf.layers.Dense(3*num_features,
                               activation=tf.nn.relu)(audio_input)
-        net = tf.layers.dropout(net, rate=0.5)
+        net = tf.layers.dropout(net, rate=0.5, training=is_training)
         net = tf.layers.Dense(3*num_features,
                               activation=tf.nn.relu)(net)
-        net = tf.layers.dropout(net, rate=0.5)
+        net = tf.layers.dropout(net, rate=0.5, training=is_training)
         net = tf.layers.Dense(latent_dim,
                               activation=tf.nn.relu)(net)
-        net = tf.layers.dropout(net, rate=0.5)
+        net = tf.layers.dropout(net, rate=0.5, training=is_training)
         net = tf.layers.Dense(num_features)(net)
         net = tf.reshape(net, (-1, 1, seq_length, num_features))
         return net
@@ -63,17 +63,17 @@ def discriminator(audio_frames=None,
         net = tf.reshape(audio_frames, (-1, num_features))
         net = tf.layers.Dense(3*num_features,
                               activation=tf.nn.relu)(net)
-        net = tf.layers.dropout(net, rate=0.5)
+        net = tf.layers.dropout(net, rate=0.5, training=is_training)
         # net = tf.layers.batch_normalization(net, training=is_training)
         net = tf.layers.Dense(3*num_features,
                               activation=tf.nn.relu)(net)
-        net = tf.layers.dropout(net, rate=0.5)
+        net = tf.layers.dropout(net, rate=0.5, training=is_training)
         # net = tf.layers.batch_normalization(net, training=is_training)
         net_l = tf.layers.Dense(latent_dim,
                               activation=tf.nn.relu)(net)
-        net = tf.layers.dropout(net_l, rate=0.5)
+        net = tf.layers.dropout(net_l, rate=0.5, training=is_training)
         net = tf.layers.Dense(num_features)(net)
-        net = tf.layers.dropout(net, rate=0.5)
+        net = tf.layers.dropout(net, rate=0.5, training=is_training)
         net = tf.layers.Dense(1)(net)
         net = tf.reshape(net, (-1, 1, seq_length, 1))
         return net, net_l

VAE_dense_train.py
@@ -47,14 +47,14 @@ def inference_net(audio_frames=None,
         audio_input = tf.reshape(audio_frames, [-1, 640])
         net = tf.layers.Dense(4*num_features,
                               activation=tf.nn.relu)(audio_input)
-        net = tf.layers.dropout(net, rate=0.5)
+        net = tf.layers.dropout(net, rate=0.5, training=is_training)
         # net = tf.layers.batch_normalization(net)
         net = tf.layers.Dense(3*num_features,
                               activation=tf.nn.relu)(net)
-        net = tf.layers.dropout(net, rate=0.5)
+        net = tf.layers.dropout(net, rate=0.5, training=is_training)
         net = tf.layers.Dense(2*num_features,
                               activation=tf.nn.relu)(net)
-        net = tf.layers.dropout(net, rate=0.5)
+        net = tf.layers.dropout(net, rate=0.5, training=is_training)
         net = tf.layers.Dense(latent_dim + latent_dim,
                               activation=tf.nn.relu)(net)
 
@@ -70,13 +70,13 @@ def generative_net(audio_frames=None,
         net = tf.reshape(audio_frames, (-1, latent_dim))
         net = tf.layers.Dense(2*num_features,
                               activation=tf.nn.relu)(net)
-        net = tf.layers.dropout(net, rate=0.5)
+        net = tf.layers.dropout(net, rate=0.5, training=is_training)
         net = tf.layers.Dense(3*num_features,
                               activation=tf.nn.relu)(net)
-        net = tf.layers.dropout(net, rate=0.5)
+        net = tf.layers.dropout(net, rate=0.5, training=is_training)
         net = tf.layers.Dense(4*num_features,
                               activation=tf.nn.relu)(net)
-        net = tf.layers.dropout(net, rate=0.5)
+        net = tf.layers.dropout(net, rate=0.5, training=is_training)
         net = tf.layers.Dense(num_features)(net)
         net = tf.reshape(net, (-1, 1, seq_length, num_features))
         return net

train.py
@@ -152,8 +152,7 @@ def train(dataset_dir=None,
             merged = tf.summary.merge_all()
             train_writer = tf.summary.FileWriter(output_dir + '/log/train/', sess.graph)
             val_writer = tf.summary.FileWriter(output_dir + '/log/validation/')
-            modal_saver = tf.train.Saver(max_to_keep=10,
-                                         keep_checkpoint_every_n_hours=1)
+            modal_saver = tf.train.Saver(max_to_keep=10)
 
             # Initialize the variables
             sess.run(tf.global_variables_initializer())
@@ -278,7 +277,7 @@ def train(dataset_dir=None,
                     val_new_metric = [val_ccc_arousal, val_ccc_valence]
 
                 # Have some penalty for the large shoot at beginning
-                if val_new_metric >= [x*0.9 for x in val_old_metric]:
+                if val_new_metric >= [x for x in val_old_metric]:
                     # Save the model
                     save_path = modal_saver.save(sess,
                                                  save_path=output_dir + "/model.ckpt",
@@ -303,6 +302,12 @@ def train(dataset_dir=None,
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--dataset_dir',
+        type=str,
+        default='./tf_records',
+        help='Path to the tensorflow records dataset'
+    )
     parser.add_argument(
         '--output_dir',
         type=str,
@@ -348,7 +353,7 @@ if __name__ == "__main__":
     FLAGS, unparsed = parser.parse_known_args()
 
     output_dir = FLAGS.output_dir
-    loss_list, dev_loss_list = train(Path("./tf_records"),
+    loss_list, dev_loss_list = train(Path(FLAGS.dataset_dir),
                                      init_learning_rate=FLAGS.learning_rate,
                                      learning_rate_decay=FLAGS.learning_rate_decay,
                                      seq_length=FLAGS.seq_length,